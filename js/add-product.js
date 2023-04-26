c36dbadf78b9fe3a01c77037f05b583bd65382eb
Smit202 <smit.sheth@simformsolutions.com>
2023-04-26 19:55:06 +0530

    completed frontend part

---
+++ js/add-product.js
@@ -0,0 +1,8 @@
+let submit = document.getElementById('submit');
+submit.addEventListener('click', () => {
+    let id = document.getElementById('prod-id');
+    let name = document.getElementById('prod-name');
+    let price = document.getElementById('prod-price');
+    let category = document.getElementById('prod-category');
+    let image = document.getElementById('prod-image');
+});
